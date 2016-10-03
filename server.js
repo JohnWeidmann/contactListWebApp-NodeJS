3c5af845d2f38103f5d2a3106180b6ba51bb4d59
JohnWeidmann <john.weidmann@mavs.uta.edu>
2016-10-03 16:29:34 -0500

    bower comps, bootstrap,jquery, angular with includes

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 
-//app.use(express.static(__dirname + "/public")); //look for static files js html css etc
-
+app.use(express.static(__dirname + '/public')); //look for static files js html css etc
+app.use('/bootstrap', express.static(__dirname + '/public/bower_components/bootstrap/dist'));
+app.use('/jquery', express.static(__dirname + '/public/bower_components/jquery/dist'));
+app.use('/angular', express.static(__dirname + '/public/bower_components/angular'));
+//app.use('bower_components',express.static(__dirname + '/bower_components')); 
 app.listen(3000);
 console.log('server running on port 3000');
