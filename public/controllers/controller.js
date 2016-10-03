956055cf64f7d42b6af05aefe3811f670b88cb97
JohnWeidmann <john.weidmann@mavs.uta.edu>
2016-10-03 18:33:29 -0500

    controllers .js with spoof data on server/controller

---
+++ public/controllers/controller.js
@@ -0,0 +1,52 @@
+//long hand//
+
+/*var app = angular.module('contactListApp', []);
+
+app.controller('contactListController', function($scope){
+	console.log("hello");
+});*/
+
+
+//short hand with dummy data//
+
+/*angular.module('contactListApp', [])
+
+.controller('contactListController', function($scope){
+	
+	person1 = {
+
+		name: 'John',
+		email: 'john@email.com',
+		number: '11111111111'
+	};
+	
+	person2 = {
+
+		name: 'Jim',
+		email: 'Jim@email.com',
+		number: '22222222222',
+
+	};
+	
+	person3 = {
+
+		name: 'James',
+		email: 'James@email.com',
+		number: '3333333333',
+
+	};
+
+	var contactList = [person1, person2,person3];
+	$scope.list = contactList;
+});*/
+
+//using server for get request
+
+angular.module('contactListApp', [])
+
+.controller('contactListController',function($scope,$http){
+			$http.get('/contactlist').success(function(response){
+				console.log("i got the data I requested"); 
+				$scope.list = response;
+			});
+		});
--- server.js
+++ server.js
@@ -1,10 +1,45 @@
 var express = require('express');
-var app = express();
+
+ var app = express();
 
 app.use(express.static(__dirname + '/public')); //look for static files js html css etc
 app.use('/bootstrap', express.static(__dirname + '/public/bower_components/bootstrap/dist'));
 app.use('/jquery', express.static(__dirname + '/public/bower_components/jquery/dist'));
 app.use('/angular', express.static(__dirname + '/public/bower_components/angular'));
-//app.use('bower_components',express.static(__dirname + '/bower_components')); 
+app.use('/controllers', express.static(__dirname + '/public/controllers'));
+
+//tells the server to listen for create contact route
+app.get('/contactlist', function(req,res){
+	
+	console.log("I recieved Get for /contactlist");
+	//for testing the request from the controller WORKS!
+	/*person1 = {
+
+		name: 'John',
+		email: 'john@email.com',
+		number: '11111111111'
+	};
+	
+	person2 = {
+
+		name: 'Jim',
+		email: 'Jim@email.com',
+		number: '22222222222',
+
+	};
+	
+	person3 = {
+
+		name: 'James',
+		email: 'James@email.com',
+		number: '3333333333',
+
+	};
+
+	var contactList = [person1, person2,person3];
+	res.json(contactList);*/
+	
+	//import from the mongodb
+});
 app.listen(3000);
 console.log('server running on port 3000');
